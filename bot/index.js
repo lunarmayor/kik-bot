7c5b023eb9e691996b3358b8c85619b2f2508086
Michael Morrissey <mikehmorrissey@gmail.com>
2016-04-06 19:54:49 -0400

    start kik bot

--- bot/index.js
+++ bot/index.js
@@ -1,3 +1,17 @@
-import './controller'
-import './responseMap'
-import './introduction'
+import http from 'http'
+import Bot from '@kikinteractive/kik'
+import request from 'request'
+
+let bot = new Bot({
+  username: process.env.BOT_USERNAME,
+  apiKey: process.env.KIK_API_KEY,
+  baseUrl: process.env.BASE_URL,
+})
+
+bot.onTextMessage((incoming, bot) => {
+ incoming.reply(incoming.body)
+})
+
+http
+  .createServer(bot.incoming())
+  .listen(8080, () => console.log('hello'))
---
+++ setup.js
@@ -0,0 +1,20 @@
+request.post({
+  url : 'https://api.kik.com/v1/config',
+  auth: {
+    'user' : process.env.BOT_USERNAME,
+    'pass' : process.env.KIK_API_KEY
+  },
+  headers:{
+    'Content-Type': 'application/json'
+  },
+  json: true,
+  body :{
+    "webhook": process.env.BASE_URL,
+    "features": {
+      "manuallySendReadReceipts": false,
+      "receiveReadReceipts": false,
+      "receiveDeliveryReceipts": false,
+      "receiveIsTyping": false
+    }
+  }
+})
